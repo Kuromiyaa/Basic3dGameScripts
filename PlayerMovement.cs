dbbf9ce3302337980b425715774bf543c78219df
Kuromiyaa <59417628+Kuromiyaa@users.noreply.github.com>
2020-04-26 01:43:03 +0300

    Updated script with jump function.

--- PlayerMovement.cs
+++ PlayerMovement.cs
@@ -1,43 +1,76 @@
-﻿using UnityEngine;
+using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour {
+public class PlayerMovement : MonoBehaviour
+{
 
-	public Rigidbody rb;
-		
-	public float forwardForce = 500f;
-	public float sidewaysForce = 500f;
+    public Rigidbody rb;
+
+    public float forwardForce = 500f;
+    public float sidewaysForce = 500f;
+    public float jumpSpeed = 10f;
+
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    
+    
+    
+    
+    
+    
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // Forward Force
+        if (Input.GetKey("w"))
+        {
+            rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+        }
+
+        // player movement
+        if (Input.GetKey("d"))
+        {
+
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("a"))
+        {
+
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("s"))
+        {
+
+            rb.AddForce(0, 0, -forwardForce * Time.deltaTime);
+        }
 
 
-	// Update is called once per frame
-	void FixedUpdate ()
-	{
-		// Forward Force
-		if (Input.GetKey ("w")) 
-		{
-			rb.AddForce (0, 0, forwardForce * Time.deltaTime);
-		}
-
-		// if osio
-		// player movement
-		if ( Input.GetKey("d"))
-		{
-
-			rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
-		}
-		if ( Input.GetKey("a"))
-		{
-
-			rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
-		}
-
-		if ( Input.GetKey("s"))
-		{
-
-			rb.AddForce(0, 0, -forwardForce * Time.deltaTime);
-		}
+        if (canJump)
+        {
+            canJump = false;
+            rb.AddForce(0, forceConst, 0, ForceMode.Impulse);
 
 
-		// Player movement done
+        }
+    }
 
-	}
-}
+
+
+
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            canJump = true;
+        }
+    }
+
+
+    public int forceConst = 50;
+
+    public bool canJump;
+
+} 
